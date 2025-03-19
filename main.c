64c556f48bbce227bc11b8700dfb0ee852a16633
LLA2007-Dev <lautaroleoneladorno2007@gmail.com>
2025-03-19 03:51:51 -0300

    Se copio de un antiguo fichero
    Se copio de un antiguo fichero de un proyecto similar el cual yo cree,
    este nuevo archivo main tiene declaradas las funciones que aun no se
    determinaron sus parametros ni sus cuerpos
    pero funciona el sistema con el cual se selecciona la accion que
    queremos realizar

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+void determinarVv();    //Determina el valor de la cuota vencida y nos da una tabla
+void determinarVa();    //Determina el valor de la cuota adelantada y nos da una tabla
+void determinarCuota(); //Determina el valor de la cuota y nos da una tabla
+
+//Declaracion de la fundion main
 int main()
 {
-    printf("Hello world!\n");
+    int operacion;
+    printf("Que operacion quieres realizar?\n\n");
+    printf("[1] - Vv (valores con cuota vencida)\n");
+    printf("[2] - Va (valores con cuota adelantada)\n");
+    printf("[3] - C (determinar la cuota)\n");
+
+    printf("\nQue operacion realizaremos: ");
+    scanf("%d",&operacion);
+
+    system("cls");
+
+    //estructura de control para determinar que funcion vamos a usar
+    switch(operacion){
+    case 1:
+        printf("DETERMINAR Vv ( VALORES DE CUOTA VENCIDA )\n");
+        break;
+    case 2:
+        printf("DETERMINAR Va ( VALORES DE CUOTA ADELANTADA ) \n");
+        break;
+    case 3:
+        printf("DETERMINAR C ( CUOTA ) \n");
+        break;
+    default:
+        printf("No ah seleccionado ninguna operacion valida \nAdios \n");
+        break;
+    }
+
+
     return 0;
 }
